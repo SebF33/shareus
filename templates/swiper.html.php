28d84d1d3a62a53e526c362305838c87c81d2a9b
SebF33 <78741537+SebF33@users.noreply.github.com>
2022-12-08 23:20:25 +0100

    templates update

--- templates/swiper.html.php
+++ templates/swiper.html.php
@@ -4,11 +4,13 @@
 <html lang="fr">
 
 <head>
-  <title>Swiper</title>
+  <title>Slides</title>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
   <meta name="Swiper" content="Swiper">
+
+  <link data-vue-tag="ssr" rel="icon" type="image/png" sizes="512x512" href="assets/favicon-512.png" />
 
   <link rel="stylesheet" href="assets/lib/bootswatch/sketchy/bootstrap.min.css" />
   <link rel="stylesheet" href="assets/lib/swiper-bundle.min.css" />
